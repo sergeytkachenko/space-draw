7a949aa31a92d45b69f0366b76239e255ba055d6
sergey <serg.tkachenko@hotmail.com>
2017-01-24 21:05:08 +0200

    edit planets

--- js/main.js
+++ js/main.js
@@ -1,20 +1,46 @@
 var canvas = document.getElementById('app');
 var space = canvas.getContext('2d');
 
-addPlanet(75, 75, 60);
-addPlanet(300, 75, 30);
+let planets = {
+	items: [],
+	
+	getByName: function(name) {
+		return planets.items.find(planet => planet.name === name);
+	},
+	
+	add: function(config) {
+		space.fillStyle = "rgba(0, 200, 200, 0.5)";
+		space.beginPath();
+		space.arc(config.x, config.y, config.r, 0, Math.PI * 2, true); 
+		space.closePath();
+		space.fill();
+	},
+	
+	getDistance: function (x1, y1, x2, y2) {
+		let x = x2 - x1;
+		let y = y2 - y1;
+		return Math.sqrt(x * x + y * y);
+	},
+
+	getPlanetDistance: function(planetName1, planetName2) {
+		let planet1 = planets.getByName(planet1);
+		let planet2 = planets.getByName(planet2);
+		return planets.getDistance(planet2.x, planet1.x, planet2.y, planet1.y);
+	}
+};
+
+planets.add({
+	name: 'earth',
+	x: 70,
+	y: 90,
+	r: 65
+}});
+
+planets.add({
+	name: 'moon',
+	x: 300,
+	y: 75,
+	r: 30
+}});
 
 
-function addPlanet(x, y, raduis) {
-	space.fillStyle = "rgba(0, 200, 200, 0.5)";
-	space.beginPath();
-	space.arc(x, y, raduis, 0, Math.PI * 2, true); 
-	space.closePath();
-	space.fill();
-}
-
-function getDistance(x1, y1, x2, y2) {
-	let x = x2 -x1;
-	let y = y2 - y1;
-	return Math.sqrt(x * x + y * y);
-}
